8b97f4200d70700e766db564110ed85f7e02d14b
Diego Giuliani <giuliani.diego@gmail.com>
2020-08-25 08:51:06 -0300

    Add video details

--- client/src/components/VideoCard.js
+++ client/src/components/VideoCard.js
@@ -6,7 +6,7 @@ export default function VideoCard({ video, onVideoSelect, onDelete }) {
   const screenshot = video.screenshot
     ? `http://localhost:3005/screenshots/${video.screenshot}`
     : 'https://dummyimage.com/420x260';
-  const duration = formatSeconds(329.2333);
+  const duration = formatSeconds(video.duration);
   return (
     <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
       <button
@@ -22,7 +22,8 @@ export default function VideoCard({ video, onVideoSelect, onDelete }) {
       <div className="flex justify-between items-center">
         <div className="mt-4">
           <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-            CATEGORY
+            {video.video_codec} /{' '}
+            {video.audio_codec ? video.audio_codec : 'No audio'}
           </h3>
           <h2 className="text-gray-900 title-font text-lg font-medium">
             {video.title}
--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -1,6 +1,6 @@
 import { format, addSeconds } from 'date-fns';
 
-export const formatSeconds = (seconds) => {
+export const formatSeconds = (seconds = 0) => {
   var helperDate = addSeconds(new Date(0), seconds);
   return format(helperDate, 'mm:ss');
 };
