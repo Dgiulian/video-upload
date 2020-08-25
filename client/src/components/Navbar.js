2c72c62d57f5578c63f86cbadca6fb534b772994
Diego Giuliani <giuliani.diego@gmail.com>
2020-08-25 07:37:43 -0300

    Styled upload button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -28,7 +28,11 @@ export default function Navbar() {
           <Link to="/" className="mr-5 hover:text-gray-900">
             Home
           </Link>
-          <Link to="/upload" className="mr-5 hover:text-gray-900">
+
+          <Link
+            to="/upload"
+            className="mr-5 inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+          >
             Upload videos
           </Link>
         </nav>
