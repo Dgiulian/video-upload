a8236f95aa62724c7cbf4b13d20dedf913b5a7e4
Diego Giuliani <giuliani.diego@gmail.com>
2020-08-24 23:18:41 -0300

    Add cors support

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 require('./db');
 
@@ -11,7 +12,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/videos');
 
 const app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
