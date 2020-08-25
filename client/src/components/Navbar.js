83eb6d292748115b8c5fd405d6fc1fb0e075ec28
Diego Giuliani <giuliani.diego@gmail.com>
2020-08-24 21:11:59 -0300

    Adjusted Navbar icon

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,15 +11,16 @@ export default function Navbar() {
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-            viewBox="0 0 24 24"
           >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            <path fill="none" d="M0 0h24v24H0z" />
+            <path d="M3 3.993C3 3.445 3.445 3 3.993 3h16.014c.548 0 .993.445.993.993v16.014a.994.994 0 01-.993.993H3.993A.994.994 0 013 20.007V3.993zM5 5v14h14V5H5zm5.622 3.415l4.879 3.252a.4.4 0 010 .666l-4.88 3.252a.4.4 0 01-.621-.332V8.747a.4.4 0 01.622-.332z" />
           </svg>
           <span className="ml-3 text-xl">Video upload</span>
         </Link>
